0d0483647c00d74724604e57682243cb797a0bfa
marcosZaragoza <marcos.zaragoza7302@gmail.com>
2025-02-27 15:51:17 -0300

    se modifico fecth mostrar igf

--- script.js
+++ script.js
@@ -143,17 +143,20 @@ if (document.getElementById('routine-container')) {
     document.getElementById("btnEjercicios").addEventListener("click", function() {
         const ejerciciosContainer = document.getElementById("ejerciciosContainer");
         ejerciciosContainer.innerHTML = "Cargando...";  // Mostrar mensaje mientras se cargan los datos
-        console.log(fetch("https://script.google.com/macros/s/AKfycbywGHo05PPEGAKRZPBV18u1vLrf6tcdLtYafhvw_tSktBaHExEjHyH2kUtgjL7gdNI0RA/exec?tipo=G"));
-        fetch("https://script.google.com/macros/s/AKfycbywGHo05PPEGAKRZPBV18u1vLrf6tcdLtYafhvw_tSktBaHExEjHyH2kUtgjL7gdNI0RA/exec?tipo=G")
+        var url1 = `https://script.google.com/macros/s/AKfycbywGHo05PPEGAKRZPBV18u1vLrf6tcdLtYafhvw_tSktBaHExEjHyH2kUtgjL7gdNI0RA/exec?tipo=G`;
+
+    // Hacer una solicitud HTTP GET a la URL
+        fetch(url1)
         .then(function(response) {
-            console.log("entro al 1er then");
-            if (!response.ok) throw new Error('Error en la respuesta del servidor');
-            console.log("despues del if");
-            return response.clone().json();  //  Clonamos para evitar conflictos con otras lecturas
-        })
-        .then(function(data) {
-            console.log(data.Array());
-            if (!Array.isArray(data)) throw new Error('La respuesta no es un array');
+            // Verificar si la respuesta es correcta (código 200-299)
+            if (response.ok) {
+                return response.json(); // Convertir la respuesta a formato JSON
+            } else {
+                throw new Error('Error en la respuesta del servidor'); // Si hay un error en la respuesta, lanzar una excepción
+            }
+        }).then(function(data) {
+            console.log("Datos recibidos:", data);
+            if (data != undefined);
             console.log("Datos recibidos:", data); 
             ejerciciosContainer.innerHTML = "";
 
