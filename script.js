17ab1c1b8fd2454d3cee72efe177bcc44c952c7c
marcosZaragoza <marcos.zaragoza7302@gmail.com>
2025-02-27 12:18:08 -0300

    se agrego el .text() para ver la info del response en funcion como se hacen

--- script.js
+++ script.js
@@ -143,7 +143,7 @@ if (document.getElementById('routine-container')) {
 
         fetch("https://script.google.com/macros/s/AKfycbywGHo05PPEGAKRZPBV18u1vLrf6tcdLtYafhvw_tSktBaHExEjHyH2kUtgjL7gdNI0RA/exec?tipo=ejerciciosGif")
         .then(function(response) {
-            console.log("que devuelve el servidor" + response);  // Verifica qué devuelve el servidor
+            console.log("que devuelve el servidor" + response.text());  // Verifica qué devuelve el servidor
             return response.json();  // Obtener los datos en formato JSON
         })
         .then(function(data) {
